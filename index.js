6ed870da2ae18c2e8fc0eb52d6b76db6913416c5
Philippe Coval <rzr@users.sf.net>
2020-03-05 17:31:16 +0100

    lib: Add github action client
    Signed-off-by: Philippe Coval <rzr@users.sf.net>
    Change-Id: Ib6f6baeb9d7e43d17e82dcd9c91fb0a80ac66b07

---
+++ index.js
@@ -0,0 +1,42 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// Copyright 2020-present Philippe Coval <http://purl.org/rzr/>
+// SPDX-Licence: ISC
+// SPDX-Licence-URL: https://spdx.org/licenses/ISC.txt
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const Mastodon = require('mastodon-lite');
+
+try {
+  var app = {};
+  app.config = {
+    access_token: '[TODO: Update with app token at https://mastodon.social/settings/applications]',
+    host: 'mastodon.social',
+    port: 443,
+    api: '/api/v1',
+    rejectUnauthorized: false
+  };
+
+  if (core.getInput('access-token'))
+    app.config.access_token = core.getInput('access-token');
+  app.config.host = core.getInput('host');
+  if (Number(core.getInput('port')))
+    app.config.port = Number(core.getInput('port'));
+  if (core.getInput('api'))
+    app.config.api = core.getInput('api');
+  if (typeof core.getInput('rejectUnauthorized') != 'undefined')
+    app.config.rejectUnauthorized = core.getInput('rejectUnauthorized');
+
+  app.mastodon = new Mastodon(app.config);
+  const message = core.getInput('message');
+  app.mastodon.post(message, (err, status) => {
+    if (err) {
+      return core.setFailed(err);
+    } else {
+      console.log(status);
+      core.setOutput('status', `succeed: ${status}`);
+    }
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
